15a80d5c85242d8732b4d02a8f1bf81e2ab39eff
Darshan Kanani <darshankanani0007@gmail.com>
2023-01-30 09:27:26 +0530

    Signed-off-by: Darshan Kanani <darshankanani0007@gmail.com>

--- screenRecorder.py
+++ screenRecorder.py
@@ -10,12 +10,13 @@ fn = input("Enter Any File Name And Path : ")
 # fix the frame rate
 fps = 30.0
 
-fourcc = c.VideoWriter_fourcc(*'XVID')
+# fourcc = c.VideoWriter_fourcc(*'XVID')
+fourcc = c.VideoWriter_fourcc('m', 'p', '4', 'v')
 output = c.VideoWriter(fn, fourcc, fps, rs)
 
 # creating recording module
 c.namedWindow("Live_Recording", c.WINDOW_NORMAL)
-c.resize("Live_Recording", (600, 400))
+# c.resize("Live_Recording", (600, 400))
 
 while True:
     img = p.screenshot()
